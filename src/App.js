3e78bea19c5262f0fc4c29f8607069e7e3725e3c
sangya2001 <sangyasherpa2058@gmail.com>
2020-11-02 21:02:17 +0545

    Login Screen for Large, Medium Screen Completed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import React from "react";
-import Logo from "./logo.png"; // remove logo
+import React from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import Login from './Components/Login/Login'
+import ThemeContext from './Contexts/ThemeContext'
 
 export default function App() {
-  // remove this
-  const style = {
-    parent: {
-      textAlign: "center",
-      fontSize: "22px",
-      margin: "3vh 4vw",
-    },
-    info: {
-      textAlign: "left",
-      margin: "0 4vw"
-    }
-  };
   return (
-    // clear the parent div
-    <div style={style.parent}>
-      <img src={Logo} alt="CodewithSudeep" style={{ width: "250px" }} />
-      <p>
-        This is a starting boilerplate for building the student management
-        system.
-      </p>
-      <hr />
-      <div style={style.info}>
-        <h3>Things to remember:</h3>
-        <ol>
-          <li>Use Raleway font for whole project.</li>
-          <li>Do not use any other color palette except given palette.</li>
-          <li>Consider using modular css, use either modular or styled component but only one.</li>
-        </ol>
-        <hr/>
-        <h3 style={{textAlign: "center"}}>Happy Hacking!</h3>
-      </div>
-    </div>
-  );
+    <ThemeContext>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+        </Switch>
+      </Router>
+    </ThemeContext>
+  )
 }
