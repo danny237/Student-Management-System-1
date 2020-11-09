76c721f06e21232170ae295fb1b4ceeaa4df1316
danny237 <dannymagar37@gmail.com>
2020-11-10 02:21:43 +0545

    home page

---
+++ src/Constants/RawData.js
@@ -0,0 +1,39 @@
+export const NoticeBoard = [
+    {
+        notice: 'College Remains Closed until further notice is conveyes.',
+        posted_at: '19th Octo, 2020, 4:15PM'
+    },
+    {
+        notice: 'College Remains Closed until further notice is conveyes.',
+        posted_at: '19th Octo, 2020, 4:15PM'
+    },
+    {
+        notice: 'College Remains Closed until further notice is conveyes.',
+        posted_at: '19th Octo, 2020, 4:15PM'
+    }
+]
+
+export const Activities = [
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+    {
+        activity: 'You sent an application for sick leave.'
+    },
+
+]
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,27 +14,53 @@ import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+
+import { NoticeBoard, Activities } from '../Constants/RawData';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useContext(DarkContext);
   const [loginStatus, setLoginStatus] = useContext(LoginContext);
 
+
+  // notices
+  let notices = NoticeBoard.map((item, index) => (
+    <>
+      <div className={`${homeStyle.noticeHolder}
+      ${darkMode ? homeStyle.lightVerticalBoder : homeStyle.darkTheme}
+      `}>
+        <div className={homeStyle.notice} key={index}>
+          {item.notice}
+              </div>
+        <div className={homeStyle.postedTime}>
+          <AccessTimeIcon fontSize="small" />
+          <p>{item.posted_at}</p>
+        </div>
+      </div>
+    </>
+  ))
+
+  // Recent Activities
+  let recentActivities = Activities.map((item, index) => (
+    <li key={index} className={homeStyle.activity}>
+      {item.activity}
+    </li>
+  ))
+
+
+
   return (
     // main page
     <div
-      className={`${homeStyle.homePage} ${
-        darkMode ? homeStyle.lightTheme : ""
-      }`}
+      className={`${homeStyle.homePage} ${darkMode ? homeStyle.lightTheme : homeStyle.darkTheme
+        }`}
     >
       {/* home sidebar */}
       <div
-        className={`${homeStyle.sidebar} ${
-          darkMode ? homeStyle.lightTheme : ""
-        }`}
+        className={`${homeStyle.sidebar} ${darkMode ? homeStyle.lightTheme : ""
+          }`}
       >
-        <div className={`${homeStyle.sidebarholder}
-          ${darkMode ? '' : homeStyle.whiteColor} 
-        `}>
+        <div className={`${homeStyle.sidebarholder}`}>
           <HouseIcon />
           <MarkunreadIcon />
           <AssignmentIcon />
@@ -48,9 +74,8 @@ export default function Home() {
       <div className={homeStyle.mainContainer}>
         {/* status bar */}
         <div
-          className={`${homeStyle.statusBar} ${
-            darkMode ? homeStyle.lightTheme : ""
-          }`}
+          className={`${homeStyle.statusBar} ${darkMode ? homeStyle.lightTheme : ""
+            }`}
         >
           {/* left section */}
           <section className={homeStyle.leftSection}>
@@ -58,7 +83,7 @@ export default function Home() {
               <h1>Nagarjuna College of IT</h1>
             </div>
             <div>
-              <h3>System Overview</h3>
+              <h3 style={{lineHeight: "25px"}}>System Overview</h3>
               <p>Last logged in 4 hours age.</p>
             </div>
           </section>
@@ -74,26 +99,68 @@ export default function Home() {
             <div className={homeStyle.statusBtn}>
               {/*  switch */}
               <Switcher />
-              <div className={homeStyle.logoutBtn}>
-                <ExitToAppIcon /> Logout
+              <div>
+                <Button
+                style={{color:`${darkMode ? '' : '#fff'}`}}
+                  onClick={() => setLoginStatus(false)}
+                >
+                  <ExitToAppIcon /> Logout
+                </Button>
               </div>
             </div>
-            
+
           </section>
         </div>
 
         {/* content */}
         <div className={`${homeStyle.content}`}>
 
-        <Button
-              onClick={() => setLoginStatus(false)}
-              variant="contained"
-              color="primary"
-            >
-              Logout
-            </Button>
+          {/* start activities */}
+          <div className={homeStyle.activities}>
+
+            <div className={homeStyle.attendance}>
+              <div className=
+              {`${homeStyle.attandenceCard}
+              ${darkMode ? homeStyle.lightHorizontalBorder : homeStyle.darkTheme}
+              `}>
+                <p>Total School Days</p>
+                <h1>285Days</h1>
+              </div>
+              <div className={`${homeStyle.attandenceCard}
+              ${darkMode ? homeStyle.lightHorizontalBorder : homeStyle.darkTheme}
+              `}>
+                <p>Total Present Days</p>
+                <h1>285Days</h1>
+              </div>
+            </div>
+
+            <div className={`${homeStyle.calender}
+            ${darkMode ? homeStyle.lightHorizontalBorder : homeStyle.darkTheme}
+            `}>
+              This is calender
+            </div>
+          </div>
+          {/* end activities */}
+
+          {/* Notice board */}
+          <div className={`${homeStyle.noticeBoard}
+          ${darkMode ? homeStyle.lightHorizontalBorder : homeStyle.darkTheme}
+          `}>
+            <h3>Notice Board</h3>
+            {notices}
+          </div>
+
+          {/* Recent Activities */}
+          <div className={`${homeStyle.recentActivities}
+          ${darkMode ? homeStyle.lightHorizontalBorder : homeStyle.darkTheme}
+          `}>
+            <h3>Recent Activities</h3>
+            {recentActivities}
+          </div>
+
         </div>
       </div>
+
     </div>
   );
 }
